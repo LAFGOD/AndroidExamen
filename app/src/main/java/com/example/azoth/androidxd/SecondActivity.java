112fb77b45f7d5d63ab2df267493a0b73d8e601d
LAFGOD <eduardo.lafoz@live.u-tad.com>
2018-01-14 15:44:19 +0100

    Tercer video mapas

--- app/src/main/java/com/example/azoth/androidxd/SecondActivity.java
+++ app/src/main/java/com/example/azoth/androidxd/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.azoth.androidxd;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,12 @@ import com.example.azoth.androidxd.Adapters.ListaMensajesAdapter;
 import com.example.azoth.androidxd.FBObjects.FBCoche;
 import com.example.azoth.androidxd.FBObjects.Mensaje;
 import com.example.milib.ListaFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.GenericTypeIndicator;
 
@@ -20,6 +26,7 @@ public class SecondActivity extends AppCompatActivity {
 
     ListaFragment listaMensajesFragment, listaFragmentCoches;
     SupportMapFragment mapFragment;
+    ArrayList<FBCoche> coches;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +41,19 @@ public class SecondActivity extends AppCompatActivity {
         listaFragmentCoches=(ListaFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentListaCoches);
 
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMapa);
+        mapFragment.getMapAsync(events);
 
-        DataHolder.instance.fireBaseAdmin.descargarYObservarRama("messages");
-        DataHolder.instance.fireBaseAdmin.descargarYObservarRama("Coches");
+        android.support.v4.app.FragmentTransaction transaction= getSupportFragmentManager().beginTransaction();
+        transaction.hide(listaFragmentCoches);
+        transaction.show(mapFragment);
+        transaction.commit();
+
+
+
+
+
+        //DataHolder.instance.fireBaseAdmin.descargarYObservarRama("messages");
+
 
         /*ArrayList<String> mdatos= new ArrayList<>();
         mdatos.add("MENSAJE 1");
@@ -52,9 +69,10 @@ public class SecondActivity extends AppCompatActivity {
 }
 
 
-class SecondActivityEvents implements FireBaseAdminListener{
+class SecondActivityEvents implements FireBaseAdminListener, OnMapReadyCallback{
 
     public SecondActivity secondActivity;
+    GoogleMap mMap;
 
     public SecondActivityEvents(SecondActivity secondActivity){
         this.secondActivity=secondActivity;
@@ -73,9 +91,9 @@ class SecondActivityEvents implements FireBaseAdminListener{
 
         }else if (rama.equals("Coches")){
             GenericTypeIndicator<ArrayList<FBCoche>> indicator=new GenericTypeIndicator<ArrayList<FBCoche>>(){};
-            ArrayList<FBCoche> coches = dataSnapshot.getValue(indicator);
+            secondActivity.coches = dataSnapshot.getValue(indicator);
 
-            ListaCochesAdapter listaCochesAdapter=new ListaCochesAdapter(coches);
+            ListaCochesAdapter listaCochesAdapter=new ListaCochesAdapter(secondActivity.coches);
             secondActivity.listaFragmentCoches.recyclerView.setAdapter(listaCochesAdapter);
 
         }
@@ -90,6 +108,19 @@ class SecondActivityEvents implements FireBaseAdminListener{
 
     @Override
     public void fireBaseAdmin_LoginOK(boolean blOK) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        DataHolder.instance.fireBaseAdmin.descargarYObservarRama("Coches");
 
     }
 }
